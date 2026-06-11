cc7b831dee189ebf4feb964a2bf625a33ada4810
Ekotto Didace <jeannotgates@gmail.com>
2026-06-11 06:07:28 +0200

    Modification du fichier 'SettingSeeder' ou j'ai changer le name_entreprise et l'email

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -15,11 +15,11 @@ class SettingSeeder extends Seeder
     {
         Setting::create([
            "user_id" => "1",
-           "name_entreprise" => "Silicon Mboa",
-           "slug" => "siliconmboa",
+           "name_entreprise" => "Rip connect",
+           "slug" => "ripconnect",
            "telephone"=> "699329171",
            "telephone_whatsapp" => "699329171",
-           "email" => "jeannotgates@gmail.com",
+           "email" => "contact@ripconnect.com",
            "om" => "699329171",
            "localisation"=>"Odza borne 10 en face de la total",
            "ville" => "Yaoundé",
